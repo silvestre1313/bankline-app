690e02a1513a597a7c3884fc6bac77c4f26d3314
Patrick <patrick.silvestre13@hotmail.com>
2022-05-11 21:37:30 -0300

    Layout tela de extratos bancarios

--- app/src/main/java/com/dio/bankline_android/ui/statement/BankStatementActivity.kt
+++ app/src/main/java/com/dio/bankline_android/ui/statement/BankStatementActivity.kt
@@ -3,10 +3,13 @@ package com.dio.bankline_android.ui.statement
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.dio.bankline_android.R
 import com.dio.bankline_android.databinding.ActivityBankStatementBinding
 import com.dio.bankline_android.databinding.ActivityWelcomeBinding
 import com.dio.bankline_android.domain.Correntista
+import com.dio.bankline_android.domain.Movimentacao
+import com.dio.bankline_android.domain.TipoMovimentacao
 import java.lang.IllegalArgumentException
 
 class BankStatementActivity : AppCompatActivity() {
@@ -27,6 +30,18 @@ class BankStatementActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        Log.d("TESTE", "chegou o ID: ${accountHolder.id} ")
+        binding.rvBankStatement.layoutManager = LinearLayoutManager(this)
+
+        findBankStatement()
+    }
+
+    private fun findBankStatement() {
+        val dataSet = ArrayList<Movimentacao>()
+        dataSet.add(Movimentacao(1 ,"11/05/22 21:24:55", "Lorem ipsum", 1000.0, TipoMovimentacao.RECEITA, 1))
+        dataSet.add(Movimentacao(1 ,"11/05/22 21:24:55", "Lorem ipsum", 1000.0, TipoMovimentacao.DESPESA, 1))
+        dataSet.add(Movimentacao(1 ,"11/05/22 21:24:55", "Lorem ipsum", 1000.0, TipoMovimentacao.RECEITA, 1))
+        dataSet.add(Movimentacao(1 ,"11/05/22 21:24:55", "Lorem ipsum", 1000.0, TipoMovimentacao.DESPESA, 1))
+        dataSet.add(Movimentacao(1 ,"11/05/22 21:24:55", "Lorem ipsum", 1000.0, TipoMovimentacao.RECEITA, 1))
+        binding.rvBankStatement.adapter = BankStatementAdapter(dataSet)
     }
 }
